211c7ed00f176bcafb39b30f8d627816c276c5d8
Nicholas Van Landschoot <nicholasvanlandschoot.mes.dev@gmail.com>
2021-03-21 01:05:11 -0600

    Added a window with basic functionality

--- Core/main.cpp
+++ Core/main.cpp
@@ -1,9 +1,41 @@
-// Create window
-
 #include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
 
+// Create window
 
 int main() 
 {
+	sf::RenderWindow window(sf::VideoMode(512, 512),"WINDOW", sf::Style::Close | sf::Style::Resize);
+
+	while (window.isOpen())
+	{
+
+		sf::Event sfEvent;
+		//This loop is consistent with the sfml event call
+		while (window.pollEvent(sfEvent))
+		{
+			switch (sfEvent.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::Resized:
+				std::cout << sfEvent.size.width << " x " << sfEvent.size.height << "\n";
+				break;
+			case sf::Event::TextEntered:
+				printf("%c",sfEvent.text.unicode);
+				break;
+			
+			}
+
+				
+		}
+
+	}			
 
 }
+
+
+
+
